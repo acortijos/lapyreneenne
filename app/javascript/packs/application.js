f492c03911b30d98f6840683c80ffdb2cd91e7a6
Adeline Cortijos <adeline.cortijos@gmail.com>
2022-01-02 18:46:40 +0100

    test

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,25 @@
+// External imports
+    import "bootstrap";
+
+    // Internal imports, e.g:
+    // import { initSelect2 } from '../components/init_select2';
+
+    document.addEventListener('turbolinks:load', () => {
+      // Call your functions here, e.g:
+      // initSelect2();
+    });
+  JS
+
+       const webpack = require('webpack');
+
+      // Preventing Babel from transpiling NodeModules packages
+      environment.loaders.delete('nodeModules');
+
+      // Bootstrap 4 has a dependency over jQuery & Popper.js:
+      environment.plugins.prepend('Provide',
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery',
+          Popper: ['popper.js', 'default']
+        })
+      );
